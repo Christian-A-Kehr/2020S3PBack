0008e1da3908ef46bc547d7640b59b4a7f01d061
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-04-29 16:06:02 +0200

    Added hash and equals override to CovidData

--- src/main/java/entities/CovidData.java
+++ src/main/java/entities/CovidData.java
@@ -10,6 +10,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.util.Date;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -199,6 +200,35 @@ public class CovidData implements Serializable
     public void setTotalDeaths(long totalDeaths)
     {
         this.totalDeaths = totalDeaths;
+    }
+    
+    @Override
+    public int hashCode()
+    {
+        return Objects.hash(this.date);
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj)
+        {
+            return true;
+        }
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final CovidData other = (CovidData) obj;
+        if (!Objects.equals(this.date, other.date))
+        {
+            return false;
+        }
+        return true;
     }
 
 }
