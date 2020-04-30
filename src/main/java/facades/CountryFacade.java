1fa973aa2473c34fa124d5752b78b1583f762670
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-04-30 12:08:06 +0200

    Added getLatestCovidEntryForCountryById method. Needs testing!

--- src/main/java/facades/CountryFacade.java
+++ src/main/java/facades/CountryFacade.java
@@ -3,8 +3,12 @@ package facades;
 import dtos.CountryBasicInDTO;
 import dtos.CountryInDTO;
 import entities.CountryData;
+import entities.CovidData;
 import errorhandling.NotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -84,12 +88,12 @@ public class CountryFacade
             {
                 throw new NotFoundException("No objects retreived from database.");
             }
-            
+
             if (query.getResultList().isEmpty())
             {
                 throw new NotFoundException("Database is empty.");
             }
-            
+
             query.getResultList().forEach((o) ->
             {
                 countryBasicDTOList.add(new CountryBasicInDTO(o));
@@ -103,32 +107,53 @@ public class CountryFacade
         }
     }
 
-//    /**
-//     * 
-//     * @param code
-//     * @return
-//     * @throws NotFoundException 
-//     */
-//    public CountryInDTO getLatestCovidEntryForCountryById(String code) throws NotFoundException
-//    {
-//        EntityManager em = getEntityManager();
-//        try
-//        {
-//            
-//            if (con == null || cov == null)
-//            {
-//                throw new NotFoundException("No object matching provided id exists in database.");
-//            }
-//            return new CountryInDTO(con, cov);
-//        }
-//        catch (IllegalArgumentException ex)
-//        {
-//            throw new NotFoundException("No object matching provided id exists in database. IllegalArgumentException.");
-//        }
-//        finally
-//        {
-//            em.close();
-//        }
-//    }
+    /**
+     *
+     * @param code
+     * @return
+     * @throws NotFoundException
+     */
+    public CountryInDTO getLatestCovidEntryForCountryById(String code) throws NotFoundException
+    {
+        EntityManager em = getEntityManager();
+        try
+        {
+            CountryData cou;
+            TypedQuery<CountryData> query = em.createQuery("SELECT cou FROM Country cou "
+                    + "WHERE cou.countrycode = :code", CountryData.class)
+                    .setParameter("code", code);
+            cou = query.getSingleResult();
+
+            if (cou == null)
+            {
+                throw new NotFoundException("No object matching provided id exists in database.");
+            }
+
+            if (!(cou.getCovidEntries().isEmpty()))
+            {
+                HashSet<CovidData> covidEntries = new HashSet<CovidData>(cou.getCovidEntries());
+                CovidData latestEntry = Collections.max(covidEntries, (CovidData o1, CovidData o2) ->
+                {
+                    return o1.getDate().compareTo(o2.getDate());
+                });
+                long newestCovidId = latestEntry.getId();
+                // previous 6 lines really need testing!
+
+                CovidData cov = em.find(CovidData.class, newestCovidId);
+
+                return new CountryInDTO(cou, cov);
+            }
+            
+            return new CountryInDTO(cou);
+        }
+        catch (IllegalArgumentException ex)
+        {
+            throw new NotFoundException("No object matching provided id exists in database. IllegalArgumentException.");
+        }
+        finally
+        {
+            em.close();
+        }
+    }
 
 }
