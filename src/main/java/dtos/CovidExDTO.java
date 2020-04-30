ceaa81ed8a4f851658f6fd6c780e1cba6381041a
MartinBrandstrup <martin.l.brandstrup@gmail.com>
2020-04-30 15:06:37 +0200

    Refactored for proper external api use

--- src/main/java/dtos/CovidExDTO.java
+++ src/main/java/dtos/CovidExDTO.java
@@ -14,20 +14,45 @@ import entities.CovidData;
 public class CovidExDTO
 {
 
+    long confirmed, deaths, recovered;
     String date;
-    long newConfirmedInfected, totalConfirmedInfected, newRecovered, totalRecovered, newDeaths, totalDeaths;
-    CountryBasicInDTO country;
 
-    public CovidExDTO(CovidData cov)
+    public CovidExDTO(long confirmed, long deaths, long recovered, String date)
     {
-        this.date = String.valueOf(cov.getDate());
-        this.newConfirmedInfected = cov.getNewConfirmedInfected();
-        this.totalConfirmedInfected = cov.getTotalConfirmedInfected();
-        this.newRecovered = cov.getNewRecovered();
-        this.totalRecovered = cov.getTotalRecovered();
-        this.newDeaths = cov.getNewDeaths();
-        this.totalDeaths = cov.getTotalDeaths();
-        this.country = new CountryBasicInDTO(cov.getCountry());
+        this.confirmed = confirmed;
+        this.deaths = deaths;
+        this.recovered = recovered;
+        this.date = date;
+    }
+
+    public long getConfirmed()
+    {
+        return confirmed;
+    }
+
+    public void setConfirmed(long confirmed)
+    {
+        this.confirmed = confirmed;
+    }
+
+    public long getDeaths()
+    {
+        return deaths;
+    }
+
+    public void setDeaths(long deaths)
+    {
+        this.deaths = deaths;
+    }
+
+    public long getRecovered()
+    {
+        return recovered;
+    }
+
+    public void setRecovered(long recovered)
+    {
+        this.recovered = recovered;
     }
 
     public String getDate()
@@ -38,87 +63,6 @@ public class CovidExDTO
     public void setDate(String date)
     {
         this.date = date;
-    }
-
-    public long getNewConfirmedInfected()
-    {
-        return newConfirmedInfected;
-    }
-
-    public void setNewConfirmedInfected(long newConfirmedInfected)
-    {
-        this.newConfirmedInfected = newConfirmedInfected;
-    }
-
-    public long getTotalConfirmedInfected()
-    {
-        return totalConfirmedInfected;
-    }
-
-    public void setTotalConfirmedInfected(long totalConfirmedInfected)
-    {
-        this.totalConfirmedInfected = totalConfirmedInfected;
-    }
-
-    public long getNewRecovered()
-    {
-        return newRecovered;
-    }
-
-    public void setNewRecovered(long newRecovered)
-    {
-        this.newRecovered = newRecovered;
-    }
-
-    public long getTotalRecovered()
-    {
-        return totalRecovered;
-    }
-
-    public void setTotalRecovered(long totalRecovered)
-    {
-        this.totalRecovered = totalRecovered;
-    }
-
-    public long getNewDeaths()
-    {
-        return newDeaths;
-    }
-
-    public void setNewDeaths(long newDeaths)
-    {
-        this.newDeaths = newDeaths;
-    }
-
-    public long getTotalDeaths()
-    {
-        return totalDeaths;
-    }
-
-    public void setTotalDeaths(long totalDeaths)
-    {
-        this.totalDeaths = totalDeaths;
-    }
-
-    public CountryBasicInDTO getCountry()
-    {
-        return country;
-    }
-
-    public void setCountry(CountryBasicInDTO country)
-    {
-        this.country = country;
-    }
-
-    @Override
-    public String toString()
-    {
-        return "CovidExDTO{" + "date=" + date + ", newConfirmedInfected="
-                + newConfirmedInfected + ", totalConfirmedInfected="
-                + totalConfirmedInfected + ", newRecovered=" + newRecovered
-                + ", totalRecovered=" + totalRecovered + ", newDeaths="
-                + newDeaths + ", totalDeaths=" + totalDeaths + ", country="
-                + country + '}';
     }
 
 }
